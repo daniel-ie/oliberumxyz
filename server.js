9465e011030645fb89feeec1d9d0e1b38e71b6fb
DominusGroup <danichg94@gmail.com>
2018-07-30 23:12:08 -0600

    ejs

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Modulos
-var bodyParser = require('body-parser');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var app = express() ;
 
+const express = require('express') ;
+var app = express() ;
+var bodyParser = require('body-parser') ; // Post
+var cookieParser = require('cookie-parser');
 const path = require('path') ;
+
 const PORT = process.env.PORT || 5000  ;
 
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, ''))) ; 
+//app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public'))) ; 
 app.set('views', path.join(__dirname, 'views')) ;
 app.set('view engine', 'ejs') ;
 app.get('/', (req, res) => res.render('pages/index'))  ; // index.html
